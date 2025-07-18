a7664349a0850849e03e333e9c463d2ab84852cd
elangenhoven <emile.langenhoven@gmail.com>
2025-07-18 05:26:33 +0200

    Add files via upload
    String Manipulation Example in Python

---
+++ replace.py
@@ -0,0 +1,7 @@
+quote = "The!quick!brown!fox!jumps!over!the!lazy!dog."
+print(quote)
+quote_replace = quote.replace("!", " ")
+print(quote_replace)
+quote_upper = quote_replace.upper()
+print(quote_upper)
+print(quote_upper[::-1])
